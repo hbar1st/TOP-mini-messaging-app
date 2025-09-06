ee7fab261775875c0b6a9467b560c74651838990
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-09-06 17:07:48 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+
+});
+
+const server = app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+server.on("error", (err) => {
+  if (err.code === "EADDRINUSE") {
+    console.error(`Port ${port} is already in use.`);
+  } else {
+    console.error("Server startup error:", err);
+  }
+  process.exit(1); // Exit the process if a critical error occurs
+});
