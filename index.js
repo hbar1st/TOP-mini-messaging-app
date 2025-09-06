be0681f8db34d84e38c5c90da5d371dee3ff5778
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-09-06 17:37:00 -0400

    fix: display messages in array on root path

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
+/**
+ * 
+ */
+app.get("/", (req, res) => {
+    res.render("index", { messages });
 });
+
+/**
+ * 
+ */
+app.get("/new", (req, res) => { });
 
 const server = app.listen(port, () => {
   console.log(`listening on port ${port}`);
