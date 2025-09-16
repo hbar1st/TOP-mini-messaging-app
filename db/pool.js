a92d84ec72d3af9b8ff3652639ea808d602ca51f
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-09-16 19:11:09 -0400

    final commit

--- db/pool.js
+++ db/pool.js
@@ -3,8 +3,6 @@ require("dotenv").config();
 
 const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 
-
-
 console.log("connection string: ", connectionString);
 
 // Add logging to see what's happening
@@ -16,7 +14,9 @@ console.log("Environment check in db/init.ts:", {
 
 const dbConfig =
 process.env.NODE_ENV === "production"
-? {
+    ? {
+      // on Railway, these environment variables need to be shared from the database service into the nodejs app block
+      // this is a manual process that must be done in the Railway dashboard (via their gui)
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 }
